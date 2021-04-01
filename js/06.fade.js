9266ee62b4443e34c9589545d92412fe9cba2818
yji00 <dksro333@naver.com>
2021-04-01 15:36:22 +0900

    v2완성

--- js/06.fade.js
+++ js/06.fade.js
@@ -1,7 +1,13 @@
 var zIdx = 1;
 
-function show(id) {
+function show(id,n) {
 	zIdx = zIdx + 1;
 	console.log(zIdx);
 	document.getElementById(id).style.zIndex = zIdx;
+	document.getElementsByClassName('pager')[0].style.color = '#ccc';
+	document.getElementsByClassName('pager')[1].style.color = '#ccc';
+	document.getElementsByClassName('pager')[2].style.color = '#ccc';
+	document.getElementsByClassName('pager')[n].style.color = '#2896f0';
 }
+
+show('banner1',0); //처음 0번값을 줌
