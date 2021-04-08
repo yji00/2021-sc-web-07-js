5c37628b0eaef3b89aa7eafafb7b882d632a179f
yji00 <dksro333@naver.com>
2021-04-08 20:26:56 +0900

    04.08

--- js/17.score.js
+++ js/17.score.js
@@ -15,96 +15,147 @@ $(document.querySelector('.wrapper'))	=> return jQuery object
 	rank: 2
 } */
 
-
-/* 글로벌 설정 */
+/******************* 글로벌 설정 *********************/
 var scores = []
 
 
+/******************* 사용자 함수 *********************/
+/* 
+if(scores[i-1].total === v.total) v.rank = scores[i-1].rank
+else v.rank = i + 1 
+if(i == 0) v.rank = 1
+else v.rank = scores[i-1].total === v.total ? scores[i-1].rank : i + 1
+*/
 
-/* 사용자 함수 */
-function scoreRender() {
-	//테이블에 점수를 등록
-	var i, htmml
+/*
+// 축약과정
+function rankingScore() {
+	scores.sort(function(a, b) {
+		return b.total - a.total 
+	})
+	scores.forEach(function(v, i) {
+		if(i === 0) v.rank = 1
+		else {
+			if(v.total === scores[i - 1].total) v.rank = scores[i - 1].rank
+			else v.rank = i + 1
+		}
+	})
+}
+
+function rankingScore() {
+	scores.sort(sortDesc('total'))
+	scores.forEach(function(v, i) {
+		if(i === 0) v.rank = 1
+		else {
+			v.rank = (v.total === scores[i - 1].total) ? scores[i - 1].rank : i + 1
+		}
+	})
+}
+
+function rankingScore() {
+	scores.sort(sortDesc('total'))
+	scores.forEach(function(v, i) {
+		v.rank = (i === 0) ? 1 : (v.total === scores[i - 1].total) ? scores[i - 1].rank : i + 1 
+	})
+}
+*/
+
+function rankingScore() {
+	scores
+	.sort(sortDesc('total'))
+	.forEach(function(v, i) {
+		v.rank = (i === 0) ? 1 : (scores[i-1].total === v.total) ? scores[i-1].rank : i + 1
+	})
+}
+
+function renderScore() {
+	// 테이블에 점수를 등록
+	var i, html
 	$('.score-tb tbody').empty()
-	for (i = 0; i < scores.length; i++) {
-
-		html = '<tr>'
-		html += '	<td>' + (i + 1) + '</td>'
-		html += '	<td>' + scores[i].name + '</td>'
-		html += '	<td>' + scores[i].kor + '점</td>'
-		html += '	<td>' + scores[i].eng + '점</td>'
-		html += '	<td>' + scores[i].math + '점</td>'
-		html += '	<td>' + scores[i].total + '점</td>'
-		html += '	<td>' + scores[i].avg + '점</td>'
-		html += '	<td>등</td>'
-		html += '	<td>'
-		html += '		<button class="bt-change">수정</button>'
-		html += '		<button class="bt-remove">삭제</button>'
-		html += '	</td>'
+	for(i=0; i<scores.length; i++) {
+		html  = '<tr>'
+		html += '<td>'+(i+1)+'</td>'
+		html += '<td>'+scores[i].name+'</td>'
+		html += '<td>'+scores[i].kor+'점</td>'
+		html += '<td>'+scores[i].eng+'점</td>'
+		html += '<td>'+scores[i].math+'점</td>'
+		html += '<td>'+scores[i].total+'점</td>'
+		html += '<td>'+scores[i].avg+'점</td>'
+		html += '<td>등</td>'
+		html += '<td>'
+		html += '<button class="bt-change">수정</button>&nbsp;'
+		html += '<button class="bt-remove">삭제</button>'
+		html += '</td>'
 		html += '</tr>'
+		// console.log(html)
 		$('.score-tb tbody').append(html)
 	}
 }
 
-
-/* 이벤트 등록 */
-
+/******************* 이벤트 등록 *********************/
 
 
-/* 이벤트 콜백 */
-//데이터 저장
+/******************* 이벤트 콜백 *********************/
 function onScoreSubmit(f) {
-	var name = f.name.value.trim()/* 문자열 앞뒤의 공백을 없애줌 */
+
+	// 데이터 저장
+	// var f = document.scoreForm
+	var name = f.name.value.trim()
 	var kor = f.kor.value.trim()
 	var eng = f.eng.value.trim()
 	var math = f.math.value.trim()
-	if (name === '') {
+	var createdAt = new Date().getTime()
+	
+	// 데이터 검증
+	if(name === '') {
 		alert('이름을 입력하세요.')
 		f.name.focus()
-		return false /* 더이상 진행하지 않음 */
+		return false
 	}
-	if (kor === '') {
+	if(kor === '') {
 		alert('국어점수를 입력하세요.')
 		f.kor.focus()
 		return false
 	}
-	if (eng === '') {
+	if(eng === '') {
 		alert('영어점수를 입력하세요.')
 		f.eng.focus()
 		return false
 	}
-	if (math === '') {
-		alert('수학점수 입력하세요.')
+	if(math === '') {
+		alert('수학점수를 입력하세요.')
 		f.math.focus()
 		return false
 	}
-	//데이터 수정
-	kor = number(kor)
-	eng = number(eng)
-	math = number(math)
+	
+	// 데이터 수정
+	kor = Number(kor)
+	eng = Number(eng)
+	math = Number(math)
 	total = kor + eng + math
-	avg = Numer((total / 3).toFixed(2))
+	avg = Number((total/3).toFixed(2))
+	// console.log(kor, eng, math, total, avg)
 
-	//데이터 배열에 저장
+	// 배열에 데이터를 저장
 	scores.push({
 		name: name,
 		kor: kor,
 		eng: eng,
 		math: math,
 		total: total,
-		avg: avg
+		avg: avg,
+		createdAt: createdAt
 	})
+	// console.log(scores)
 
-	console.log(scores)
-
-	//form초기화
+	// form을 초기화
 	f.reset()
 
-	//데이터 화면
-	scoreRender()
+	// 데이터의 순위를 가공한다
+	rankingScore()
+
+	// 데이터를 화면에 표현한다
+	renderScore()
 
 	return false
 }
-
-
-
